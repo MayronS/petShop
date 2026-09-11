271a72e6833c3addff5c60904626fb5210fd6e12
Mayron <80633811+MayronS@users.noreply.github.com>
2026-09-10 21:44:09 -0300

    Refactor PetShop class with new structure and methods

--- petshop/src/PetShop.java
+++ petshop/src/PetShop.java
@@ -1,29 +1,10 @@
 public class PetShop {
-    private int aguaNaMaquina = 30;
-    private int shampooNaMaquina = 10;
-
-    public void abastecerAgua(){
-        if (aguaNaMaquina < 30){
-            if (2 + aguaNaMaquina > 30){
-                System.out.println("limite excedido. Não é possivel abastecer no momento!");
-            }
-            else {
-                System.out.println("Abastecimento realizado com sucesso!");
-                aguaNaMaquina += 2;
-            }
-        }
-
-    }
-    public void shampoo(){
-        System.out.println("Quanto você deseja abastecer?");
-        if (2 + shampooNaMaquina > 10){
-            System.out.println("limite excedido! Não é possivel abastecer no momento!");
-        }
-        else {
-            System.out.println("Abastecimento realizado com sucesso!");
-            shampooNaMaquina += 2;
-        }
-    }
+    private boolean petNaMaquina = false;
+    private boolean maquinaLimpa = true;
+    private int adicionarAgua;
+    private int adicionarShampoo;
+    private int aguaNaMaquina;
+    private int shampooNaMaquina;
 
     public int getAguaNaMaquina() {
         return aguaNaMaquina;
@@ -33,13 +14,82 @@ public class PetShop {
         return shampooNaMaquina;
     }
 
-    public void banhoNoPet(){
-        this.aguaNaMaquina -= 10;
-        this.shampooNaMaquina -= 2;
+
+
+    public PetShop(){}
+    public PetShop(int adicionarAgua, int adicionarShampoo) {
+        this.adicionarAgua = adicionarAgua;
+        this.adicionarShampoo = adicionarShampoo;
+        aguaNaMaquina = adicionarAgua;
+        shampooNaMaquina = adicionarShampoo;
     }
-    public void LimparMaquina(){
-        this.aguaNaMaquina -= 3;
-        this.shampooNaMaquina -= 1;
+
+    public void abastecerAgua(){
+        System.out.println("Abastecimento realizado com sucesso!");
+        aguaNaMaquina += adicionarAgua;
+
+    }
+    public void abastecerShampoo(){
+        System.out.println("Abastecimento realizado com sucesso!");
+        shampooNaMaquina += adicionarShampoo;
+
+    }
+
+
+    public void banhoNoPet(){
+        if (getShampooNaMaquina() < 2 || getAguaNaMaquina() < 10){
+            System.out.println("Não tem produto suficiente para dar banho no pet! Favor abastecer!");
+        } else if (!petNaMaquina) {
+            System.out.println("Não tem pet na maquina para dar banho");
+        } else {
+            System.out.println("O pet está tomando banho");
+            maquinaLimpa = false;
+            this.aguaNaMaquina -= 10;
+            this.shampooNaMaquina -= 2;
+
+        }
+
+    }
+    public void temPetBanhando(){
+
+        if (petNaMaquina){
+            System.out.println("Já tem um pet no banho");
+        }
+        else System.out.println("Não tem pet no banho");
+    }
+    public void colocarPet(){
+        if(petNaMaquina){
+            System.out.println("Já tem um pet no banho");
+        } else if (!maquinaLimpa) {
+            System.out.println("Maquina está suja, precisa ser limpa primeiro!");
+
+        } else{
+            System.out.println("Pet colocado na maquina");
+            petNaMaquina = true;
+        }
+    }
+    public void retirarPet(){
+        if(!petNaMaquina){
+            System.out.println("Não tem nenhum pet na maquina");
+        }
+        else {
+            System.out.println("Pet retirado da maquina");
+            petNaMaquina = false;
+        }
+    }
+    public void limparMaquina(){
+        if(petNaMaquina){
+            System.out.println("Não é possivel limpar a maquina no momento tem um pet nela.");
+        } else if (getAguaNaMaquina() < 3 || getShampooNaMaquina() < 1) {
+            System.out.println("Não tem produtos suficientes para limpar a maquina. Favor abastecer!");
+        }
+        else {
+            System.out.println("Maquina limpa com sucesso!");
+            this.aguaNaMaquina -= 3;
+            this.shampooNaMaquina -= 1;
+
+            maquinaLimpa = true;
+        }
     }
 
 }
